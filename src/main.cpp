0d1a91ea263b65554e44c0e3c2a9de76ee1fbec4
zoe wall <zoe@zwall.co.uk>
2017-10-01 18:51:32 +0100

    WORKING UNIX BUILD WITH OPENGL CONTEXT!

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,48 @@
 //
 
 #include <iostream>
+#include "engine.h"
+#include "entity_manager.h"
+#include "physics_system.h"
+#include "renderer.h"
+#include "state_machine.h"
+#include "engine_states.h"
+#include "input_handler.h"
+
+
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
 #include <GLFW/glfw3.h>
 
+using namespace std;
+// Include GLM
+#include <glm/glm.hpp>
+using namespace glm;
 
-int main()
-{
+
+int main() {
+
     std::cout << "hello world" << std::endl;
-    glfwInit();
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Race The Window", nullptr, nullptr);
+    auto eng = engine::get();
+    eng->add_subsystem("entity_manager", entity_manager::get());
+    eng->add_subsystem("physics_system", physics_system::get());
+    eng->add_subsystem("renderer", renderer::get());
+    eng->add_subsystem("state_machine", engine_state_machine::get());
+    eng->add_subsystem("input_handler", input_handler::get());
 
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
+    engine_state_machine::get()->add_state("1", std::make_shared<engine_state_1>());
+    engine_state_machine::get()->add_state("2", std::make_shared<engine_state_2>());
+    engine_state_machine::get()->change_state("1");
 
-    glfwDestroyWindow(window);
+    auto e = entity_manager::get()->create_entity("Test");
+    e->add_component("physics", physics_system::get()->build_component(e));
+    e->add_component("render", renderer::get()->build_component(e, "Blue", "Sphere", "Gouraud"));
 
-    glfwTerminate();
+    eng->run();
 
     return 0;
 }
