5827230236a73ffed5c2e05ec207a61d11c478d6
Manos Miras <prezolov@live.com>
2017-10-06 16:37:19 +0100

    Forgot to add some files

---
+++ src/engine_state_machine.cpp
@@ -0,0 +1,140 @@
+#include "engine_state_machine.h"
+
+engine_state_machine::engine_state_machine()
+{
+	_visible = false;
+}
+
+std::shared_ptr<engine_state_machine> engine_state_machine::get()
+{
+	static std::shared_ptr<engine_state_machine> instance(new engine_state_machine());
+	return instance;
+}
+
+void engine_state_machine::add_state(const std::string &name, std::shared_ptr<engine_state> state, state_type type)
+{
+	state->type = type;
+	_states[name] = state;
+}
+
+void engine_state_machine::change_state(const std::string &name)
+{
+	if (name != _current_state_name)
+	{
+		auto found = _states.find(name);
+		if (found != _states.end())
+		{
+			if (_current_state != nullptr)
+				_current_state->on_exit();
+			_current_state = found->second;
+			_current_state->on_enter();
+			_current_state_name = name;
+			_current_state_type = _current_state->type;
+		}
+		else
+		{
+			throw std::invalid_argument("state not found");
+		}
+	}
+}
+
+std::string engine_state_machine::get_current_state()
+{
+	return _current_state_name;
+}
+
+state_type engine_state_machine::get_current_state_type()
+{
+	return _current_state_type;
+}
+
+bool engine_state_machine::initialise()
+{
+	return true;
+}
+
+bool engine_state_machine::load_content()
+{
+	return true;
+}
+
+void engine_state_machine::update(float delta_time)
+{
+	static int escape_old_state = GLFW_RELEASE;
+	int escape_state = glfwGetKey(glfw::window, GLFW_KEY_ESCAPE);
+	
+	if (escape_state == GLFW_RELEASE && escape_old_state == GLFW_PRESS)
+	{
+		switch (engine_state_machine::get()->get_current_state_type())
+		{
+			// Go from game state to pause state
+			case state_type::GAME:
+				engine_state_machine::get()->change_state("pause_state");
+				break;
+			// Go from pause state to game state
+			case state_type::PAUSE:
+				engine_state_machine::get()->change_state("game_state");
+				break;
+			default:
+				// do nothing
+				break;
+		}
+	}
+	escape_old_state = escape_state;
+
+	static int enter_old_state = GLFW_RELEASE;
+	int enter_state = glfwGetKey(glfw::window, GLFW_KEY_ENTER);
+
+	if (enter_state == GLFW_RELEASE && enter_old_state == GLFW_PRESS)
+	{
+		switch (engine_state_machine::get()->get_current_state_type())
+		{
+			// Go from menu state to game state
+			case state_type::MENU:
+				engine_state_machine::get()->change_state("game_state");
+				break;
+			default:
+				// do nothing
+				break;
+		}
+	}
+	enter_old_state = enter_state;
+
+	static int backspace_old_state = GLFW_RELEASE;
+	int backspace_state = glfwGetKey(glfw::window, GLFW_KEY_BACKSPACE);
+
+	if (backspace_state == GLFW_RELEASE && backspace_old_state == GLFW_PRESS)
+	{
+		switch (engine_state_machine::get()->get_current_state_type())
+		{
+			// Go from pause state to menu state
+		case state_type::PAUSE:
+			engine_state_machine::get()->change_state("menu_state");
+			break;
+		default:
+			// do nothing
+			break;
+		}
+	}
+	backspace_old_state = backspace_state;
+
+
+	if (_current_state != nullptr)
+		_current_state->on_update(delta_time);
+
+}
+
+void engine_state_machine::render()
+{
+
+}
+
+void engine_state_machine::unload_content()
+{
+
+}
+
+void engine_state_machine::shutdown()
+{
+
+}
