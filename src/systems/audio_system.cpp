6e79aca53c3d5ad6f61a4041d0a7d051b73b4ea2
Emmanuel Miras <prezolov@live.com>
2017-11-15 21:26:45 +0000

    trying to get sound working with tinysound

--- src/systems/audio_system.cpp
+++ src/systems/audio_system.cpp
@@ -1,6 +1,8 @@
 #include "audio_system.h"
+#define TINYSOUND_IMPLEMENTATION
+#include "../tinysound.h"
+#include <stdio.h>
 
-using namespace glm;
 
 audio_system::audio_system()
 {
@@ -10,6 +12,8 @@ audio_system::audio_system()
 bool audio_system::initialise()
 {
 	std::cout << "Audio system initialising" << std::endl;
+	HWND hwnd = GetConsoleWindow();
+	ctx = tsMakeContext(hwnd, 44000, 15, 0, 1000);
 	return true;
 }
 
@@ -21,7 +25,48 @@ bool audio_system::load_content()
 
 void audio_system::update(float delta_time)
 {
+	static float total_time;
+	total_time += delta_time;
+
+	//if (ready_to_mix)
+	//tsLock(ctx);
+
+
+	tsMix(ctx);
+
 	//std::cout << "Audio system updating" << std::endl;
+}
+
+void audio_system::play_one_shot(std::string path, int duration)
+{
+	//HWND hwnd = GetConsoleWindow();
+	//ctx = tsMakeContext(hwnd, 44000, 15, duration, 1);
+
+	tsLoadedSound loaded = tsLoadWAV(path.c_str());
+	tsPlaySoundDef def = tsMakeDef(&loaded);
+	tsPlayingSound* sound = tsPlaySound(ctx, def);
+
+	//ready_to_mix = true;
+	//tsMix(ctx);
+
+
+	//tsThreadSleepDelay(ctx, 10);
+
+	//tsLoadedSound loaded = tsLoadWAV(path.c_str());
+	//tsPlayingSound sound = tsMakePlayingSound(&loaded);
+	//tsSpawnMixThread(ctx);
+	//tsSleep(500);
+	//tsInsertSound(ctx, &sound);
+	//tsInsertSound(ctx, &sound);
+	//tsPlaySoundDef def = tsMakeDef(&loaded);
+	//tsPlaySound(ctx, def);
+	//tsMix(ctx);
+	//tsSleep(duration * 1000);
+	//tsShutdownContext(ctx);
+	//tsStopSound(&sound);
+	//tsStopAllSounds(ctx);
+
+	tsFreeSound(&loaded);
 }
 
 void audio_system::render()
--- src/systems/audio_system.h
+++ src/systems/audio_system.h
@@ -1,18 +1,18 @@
-#pragma once
-
+#ifndef RACETHEMOON_AUDIO_COMPONENT_H
+#define RACETHEMOON_AUDIO_COMPONENT_H
 #include <iostream>
 #include <vector>
 #include <string>
-#include "../entity.h"
 #include "../subsystem.h"
-#include "../entity_manager.h"
-
+//#define TINYSOUND_IMPLEMENTATION
+#include "../tinysound.h"
 class audio_system : public subsystem
 {
 private:
-
+	tsContext* ctx;
+	bool ready_to_mix = false;
 public:
-
+	
 	audio_system();
 
 	static inline std::shared_ptr<audio_system> get()
@@ -20,6 +20,8 @@ public:
 		static std::shared_ptr<audio_system> instance = std::shared_ptr<audio_system>(new audio_system());
 		return instance;
 	}
+
+	void play_one_shot(std::string path, int duration);
 
 	bool initialise() override final;
 
@@ -33,3 +35,4 @@ public:
 
 	void shutdown() override final;
 };
+#endif // RACETHEMOON_AUDIO_COMPONENT_H
